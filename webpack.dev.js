dbb49635cd83a9eaa78226ed73835099ea9e7f26
Bryce <bryce_davidson@icloud.com>
2020-08-26 12:25:47 -0700

    Working - Added ts-loader and module rules

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,22 +4,33 @@ const path = require("path");
 module.exports = {
     mode: "development",
     entry: {
-        main: "./src/index.js"
+        main: "./src/index.ts"
+    },
+    module: {
+        rules: [
+            {
+                // test the file, if passes->run 'use' loader
+                test: /\.ts$/,
+                use: "ts-loader",
+                //    These are the folders to check for a rule pass
+                include: [path.resolve(__dirname, "src")]
+            }
+        ] 
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Development",
+            template: "./template.html"
+        })
+    ],
+    output: {
+        filename: "[name].[contentHash].bundle.js",
+        path: path.resolve(__dirname, "dist")
     },
     devtool: "inline-source-map",
     devServer: {
         contentBase: './dist',
         clientLogLevel: 'info',
         port: 8080
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "Development",
-            template: "./html.template"
-        })
-    ],
-    output: {
-        filename: "[name].[contentHash].bundle.js",
-        path: path.resolve(__dirname, "dist")
     },
 }
